4268756556fb3a1b588a761d9b4c07935be61868
Razka0517 <87798306+Razka0517@users.noreply.github.com>
2023-11-25 09:37:11 +0700

    Add files via upload

--- sistemlogin.py
+++ sistemlogin.py
@@ -1,8 +1,3 @@
-from os import access, name
-from typing import Optional
-import time
-import datetime
-
 def login(name,password) :
     sukses = False
     file = open("databaselogin.txt","r")
@@ -14,19 +9,36 @@ def login(name,password) :
             break
     file.close()
     if (sukses) :
-        print("login berhasil! selamat datang, "+name)
-        int(input("Masukkan kode billing yang akan anda beli"))
+        print("\nlogin berhasil! selamat datang, "+name)
+
     else :
         print("Akun anda belum terdaftar, silahkan lakukan registrasi akun !")
+
+def loginadm(name,password) :
+    sukses = False
+    file = open("databaseadmin.txt","r")
+    for i in file :
+        a,b=i.split(",")
+        b = b.strip()
+        if (a == name and b == password) :
+            sukses = True
+            break
+    file.close()
+    if (sukses) :
+        print("Selamat datang,",name)
+
+    else :
+        print("Password salah !!")
+
+waktu = 0
 
 def registrasi(name,password) :
     """ a untuk append (menambahkan)"""
     file = open("databaselogin.txt","a")
-    file.write("\n"+name+","+password)
+    file.write("\n"+name+","+password,+waktu)
     
-def billing(bill) :
-    file = open("databaselogin.txt","a")
-    file.write("\n",)
+def border() :
+    print("*"*40)
 
 def access(option) :
     global name 
@@ -34,6 +46,11 @@ def access(option) :
         name = input("Masukkan nama atau ID : ")
         password = input("Masukkan Password : ")
         login(name,password)
+    elif (option == "admin") :
+        print("==============Menu admin==============")
+        name="admin"
+        password=input("Masukkan Password : ")
+        loginadm(name,password)
     else :
         print("Masukkan ID dan Password anda yang ingin di buat : ")
         name = input("Masukkan ID : ")
@@ -43,11 +60,14 @@ def access(option) :
 
 def begin () :
     global option
-    print("Selamat datang !!")
+    print("Selamat datang di zero.net")
+    border()
     print("ketik 'login' jika anda ingin login")
     print("ketik 'reg' jika anda ingin registrasi")
+    print("ketik 'admin' untuk login sebagai admin")
+    border()
     option = str(input("Masukkan opsi yang ada diatas : "))
-    if (option !="login" and option !="reg") :
+    if (option !="login" and option !="reg" and option !="admin") :
         begin()
 
 
